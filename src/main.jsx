08df65641ac45ddee3c14066ede0d227cb958496
User Friendly <pivanov@user-friendly.dev>
2024-09-07 13:52:13 -0500

    Clear client console on module updates

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,12 @@
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 
+if (import.meta.hot) {
+  import.meta.hot.on(
+    "vite:beforeUpdate",
+    /* eslint-disable-next-line no-console */
+    () => console.clear()
+  );
+}
+
 ReactDOM.createRoot(document.getElementById("root")).render(<App />);
