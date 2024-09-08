457aa01f2a9d04bb82568256bc7cc98a4251ee25
User Friendly <pivanov@user-friendly.dev>
2024-09-08 15:09:17 -0500

    Reset app.

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,6 @@
-import MyComponent from './MyComponent'
-
 const App = () => {
   return <div>
-  	<MyComponent bgColor="Yellow" height={90}>
-		This is some free text.
-	</MyComponent>
-	
-	<MyComponent bgColor="Lavender" height={40}>
-		<p>This is a paragraph.</p>
-		<p>This is another paragraph.</p>
-	</MyComponent>
-	
-	<MyComponent />
+  	todo
   </div>
 }
 
--- src/MyComponent.jsx
+++ src/MyComponent.jsx
@@ -1,3 +1,20 @@
+/**
+ * Sample usage:
+ * 
+ *
+	<MyComponent bgColor="Yellow" height={90}>
+		This is some free text.
+	</MyComponent>
+	
+	<MyComponent bgColor="Lavender" height={40}>
+		<p>This is a paragraph.</p>
+		<p>This is another paragraph.</p>
+	</MyComponent>
+	
+	<MyComponent />
+ * 
+ */
+
 const MyComponent = (props) => {
 	const {children, bgColor, height} = props
 	 
