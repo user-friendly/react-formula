5903ebde90185c13c62ac889c4da12861dee70c4
User Friendly <pivanov@user-friendly.dev>
2024-09-08 14:20:08 -0500

    Passing props.

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
-const Title = () => {
-	return <div>
-		Hello World
+const Message = (props) => {
+	const {text, bgColor} = props	
+	return <div style={{
+		background: bgColor
+	}}>
+		{text}
 	</div>
 }
 
 const App = () => {
-  return <div>
-  	<Title />
-    todo
-  </div>;
-};
+  return <div style={{border: "1pt solid black"}}>
+  	<Message text="Sky" bgColor="cyan" />
+	<Message text="Grass" bgColor="green" />
+	<Message text="Strawberry" bgColor="red" />
+  </div>
+}
 
 export default App;
