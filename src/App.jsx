bff8c9519d1dee3179fe3904d508030c77411e55
User Friendly <pivanov@user-friendly.dev>
2024-09-09 09:10:27 -0500

    Tailwind CSS basics.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,15 @@
 const App = () => {
   return <div>
-  	todo
+  	<div className="text-rose-300 bg-red-700">One</div>
+	<div className="text-gray-200 bg-slate-600 border-y-black border-x-blue-600 border-x-8 border-y-2">
+		Two
+	</div>
+	<div className="text-2xl bg-emerald-200 text-blue-600">Three</div>
+	
+	<div className="text-xl text-purple-500">
+		<div className="border-4 border-sky-500">Four</div>
+		<div className="bg-violet-200 border-l-8 border-l-purple-800">Five</div>
+	</div>
   </div>
 }
 
