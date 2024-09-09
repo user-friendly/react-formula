127a922326b1f1ef73d7363fb71f2614277e036f
User Friendly <pivanov@user-friendly.dev>
2024-09-09 11:19:44 -0500

    Having some Tailwind CSS layout fun!

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,85 @@
-const App = () => {
-  return <div>
-  	<div className="text-rose-300 bg-red-700">One</div>
-	<div className="text-gray-200 bg-slate-600 border-y-black border-x-blue-600 border-x-8 border-y-2">
-		Two
+import { Fragment } from 'react';
+
+const Button = (props) => {
+	const {children} = props
+	// TODO Use Router instead.
+	return <a href="#" className="
+		select-none
+		cursor-pointer
+		
+		px-2.5
+		
+		rounded-xl
+		bg-sky-400
+		
+		transition-bg
+	">
+		{children}
+	</a>
+}
+
+const NavBar = (props) => {
+	const {children} = props
+	return <div className="
+		text-lg
+		font-semibold
+		font-mono
+		
+		text-stone-800
+		
+		p-2
+		
+		flex flex-row flex-wrap gap-2
+		place-content-center
+  	 ">
+		{children}
 	</div>
-	<div className="text-2xl bg-emerald-200 text-blue-600">Three</div>
+}
+
+const App = () => {
+  return <div className="flex flex-col pt-6">
+  	<NavBar>
+		<Button>Home</Button>
+		<Button>Portfolio</Button>
+		<Button>Resume</Button>
+		<Button>About</Button>
+		
+		{/*
+		<Button>Item 1</Button>
+		<Button>Item 2</Button>
+		<Button>Item 3</Button>
+		<Button>Item 4</Button>
+		*/}
+	</NavBar>
 	
-	<div className="text-xl text-purple-500">
-		<div className="border-4 border-sky-500">Four</div>
-		<div className="
-			w-1/4 min-w-min
-			select-none
-			cursor-pointer
-			transition-border-color
-			duration-1000
-			bg-violet-200 border-l-8
-			border-l-purple-800
-			hover:border-purple-300
-		">
-			Five
-		</div>
+	{/* TODO Centering content with margin x-axis auto at medium breakpiont, is this a good idea? */}
+	<div className="
+		text-sans
+		
+		w-auto
+		md:w-1/2
+		md:mx-auto
+		
+		p-6
+		mx-6 my-8
+		
+		rounded-xl bg-slate-300
+	">
+		<p>This is just some random text. The first paragraph of many to come.</p>
+		<p>Not really that many to come. Just a couple more to come.</p>
+		<p>It was a warm spring morning when the sun rose over the misty mountains.</p>
+	</div>
+	
+	<div>
+		<NavBar>
+			<Button>Site Map</Button>
+			<Button>Contact</Button>
+		</NavBar>
+		
+		<NavBar>
+			<Button>Terms & Conditions</Button>
+			<Button>Privacy Policy</Button>
+		</NavBar>
 	</div>
   </div>
 }
