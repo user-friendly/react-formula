aa92363813f46b4a5431260672186f9daa45d532
User Friendly <pivanov@user-friendly.dev>
2024-09-17 18:30:50 -0500

    Add lesson template page.

---
+++ src/Page/Lesson/LessonTemplate.jsx
@@ -0,0 +1,20 @@
+/**
+ * [Lesson title.]
+ * 
+ * Path: [path to lesson]
+ */
+
+import Router from '#Router'
+
+const LessonTemplate = () => {
+	
+	return <div className="h-full">
+		[This is a generic lesson template. Replace this with conent.]
+	</div>
+}
+
+// TODO Uncomment these and replace with appropriate paths.
+// Router.addRoute('/lesson/template', <LessonTemplate />, 'Lesson Template')
+// Router.addRoute('/lesson', <SearchPageDesign />)
+
+export default LessonTemplate
