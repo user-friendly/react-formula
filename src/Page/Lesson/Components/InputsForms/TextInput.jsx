acc4e9794a3050f18aab2fbe9bbb99b480ad30e8
User Friendly <pivanov@user-friendly.dev>
2024-09-30 13:54:10 -0500

    Implement Input Text lesson.

--- src/Page/Lesson/Components/InputsForms/TextInput.jsx
+++ src/Page/Lesson/Components/InputsForms/TextInput.jsx
@@ -1,7 +1,12 @@
 
+import { useState } from 'react'
+
 export default ({children}) => {
-	return <div className="md:ml-48 md:max-w-xl">
-		[TextInput]
+	const [text, setText] = useState('Hello Input')
+	
+	return <div className="mt-4 flex flex-col justify-center items-center">
+		<input className="bg-orange-100 rounded-lg p-2 text-neutral-700 border-2 border-neutral-300 focus:border-green-400 outline-none"
+			type="text" value={text} onChange={e => setText(e.target.value)} />
+		<span>ChatBot says: {text}</span>
 	</div>
 }
-
