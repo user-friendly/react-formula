465890ba511faeaac2403a55fe1d2dfd3e4cafa9
User Friendly <pivanov@user-friendly.dev>
2024-10-01 09:07:55 -0500

    Add Word Filter lesson skeli.

--- src/Page/Lesson/InputsForms.jsx
+++ src/Page/Lesson/InputsForms.jsx
@@ -10,7 +10,7 @@ import Collapsable from './Components/Collapsable'
 
 import TextInput from './Components/InputsForms/TextInput'
 import SignInForm from './Components/InputsForms/SignInForm'
-//import TextInput from './Components/InputsForms/TextInput'
+import WordFilter from './Components/InputsForms/WordFilter'
 //import TextInput from './Components/InputsForms/TextInput'
 //import TextInput from './Components/InputsForms/TextInput'
 //import TextInput from './Components/InputsForms/TextInput'
@@ -21,8 +21,10 @@ const InputsForms = ({children}) => {
 	const lessons = [
 		<Collapsable key={i++} collapse={true} title='Text Input'>
 			<TextInput /></Collapsable>,
-		<Collapsable key={i++} collapse={false} title='Sign In Form'>
-			<SignInForm /></Collapsable>
+		<Collapsable key={i++} collapse={true} title='Sign In Form'>
+			<SignInForm /></Collapsable>,
+		<Collapsable key={i++} collapse={false} title='Word Filter'>
+			<WordFilter /></Collapsable>
 	]
 	
 	return <div className="h-full flex flex-col justify-start items-strech">
