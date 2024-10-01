fa0a925e12bfc8cee3c8bde69af634cb03895ec6
User Friendly <pivanov@user-friendly.dev>
2024-10-01 13:25:01 -0500

    Add regexp to Word Filter search.

--- src/Page/Lesson/Components/InputsForms/WordFilter.jsx
+++ src/Page/Lesson/Components/InputsForms/WordFilter.jsx
@@ -1,7 +1,7 @@
 
 import _ from 'lodash'
 
-// import { useState, useEffect, useId } from 'react'
+import { useState, useId } from 'react'
 
 import TextInput from './Components/TextInput'
 
@@ -9,26 +9,52 @@ const WordItem = ({word}) => {
   return <div className="px-1 m-1 text-sm text-yellow-600 bg-yellow-200 border border-yellow-300">{word}</div>
 }
 
+const regex = /^\/(.+)\/([dgimsuvy]*)$/g
+
 export default ({children}) => {
+	const [filter, setFilter] = useState('')
+	
 	const submitHandler = (e) => {
 		e.preventDefault()
-		console.log(`Post form to API backend.`)
+		console.log(`Post form to API backend: ${filter}`)
 	}
 	
+	let search = filter.trim().match(regex)
+	if (_.isArray(search) && search.length > 0) {
+		try {
+			search = search[0].matchAll(regex).toArray()[0]
+			search = new RegExp(search[1], search[2])
+		} catch (e) {
+			console.log('error: invalid regexp')
+			search = filter.trim()
+		}
+	}
+	else {
+		search = filter.trim()
+	}
 	const worditems = words.map((w, i) => {
-		return <WordItem word={w} key={i} />
+		if (_.isRegExp(search)) {
+			if (search.test(w)) {
+				return <WordItem word={w} key={i} /> 
+			}
+		}
+		else if (_.isString(search)) {
+			if (search.length == 0 || w.toLowerCase().includes(search)) {
+				return <WordItem word={w} key={i} />
+			}
+		}
 	})
 	
 	return <div className="flex flex-col items-center">
 		<form method="post" action="/" onSubmit={submitHandler}
 			className="my-4"
 		>
-			<TextInput name="search" placeholder="enter a string" delay={500}
-				className="px-4 py-2 bg-neutral-300 rounded-xl rounded-r-none outline-none
-					border-2 border-r-0 border-transparent focus:border-green-400
-				"
+			<TextInput value={filter} onChange={v => setFilter(v)} delay={500}
+				name="search" placeholder="enter a string or /regexp/i"
+				className="w-60 px-4 py-2 bg-neutral-300 rounded-xl rounded-r-none outline-none
+					border-2 border-r-0 border-transparent focus:border-green-400"
 			/>
-			<input type="submit" value="search"
+			<input id={useId()} type="submit" value="search"
 				className="px-4 py-2 bg-green-400 text-white rounded-xl rounded-l-none cursor-pointer border-2 border-l-0 border-green-400"
 			/>
 		</form>
