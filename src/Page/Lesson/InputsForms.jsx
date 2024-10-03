8fee19ecc648be09783db15306bc237fedac16f0
User Friendly <pivanov@user-friendly.dev>
2024-10-03 16:11:24 -0500

    Add Messages Project view skeli.

--- src/Page/Lesson/InputsForms.jsx
+++ src/Page/Lesson/InputsForms.jsx
@@ -11,7 +11,7 @@ import Collapsable from './Components/Collapsable'
 import TextInput from './Components/InputsForms/TextInput'
 import SignInForm from './Components/InputsForms/SignInForm'
 import WordFilter from './Components/InputsForms/WordFilter'
-//import TextInput from './Components/InputsForms/TextInput'
+import MessageProject from './Components/InputsForms/MessageProject'
 //import TextInput from './Components/InputsForms/TextInput'
 //import TextInput from './Components/InputsForms/TextInput'
 //import TextInput from './Components/InputsForms/TextInput'
@@ -23,8 +23,10 @@ const InputsForms = ({children}) => {
 			<TextInput /></Collapsable>,
 		<Collapsable key={i++} collapse={true} title='Sign In Form'>
 			<SignInForm /></Collapsable>,
-		<Collapsable key={i++} collapse={false} title='Word Filter'>
-			<WordFilter /></Collapsable>
+		<Collapsable key={i++} collapse={true} title='Word Filter'>
+			<WordFilter /></Collapsable>,
+		<Collapsable key={i++} collapse={false} title='Message Project'>
+			<MessageProject /></Collapsable>
 	]
 	
 	return <div className="h-full flex flex-col justify-start items-strech">
