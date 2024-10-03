ffdc6409a61a42889d7eb53de55865008fa9d2c2
User Friendly <pivanov@user-friendly.dev>
2024-10-03 18:25:36 -0500

    Implement Message Project.

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,22 @@ export default {
 		fontFamily: {
 			// Font is directly linked in the index.html document.
 			noto: ['Noto Color Emoji', 'sans-serif']
+		},
+		
+		keyframes: {
+			slideDownKF: {
+				'from': {
+					transform: 'translateY(-100%)',
+				    opacity: 0,
+				},
+				'to': {
+					transform: 'translateY(0)',
+				    opacity: 1,
+				},
+			}
+		},
+		animation: {
+			slideDown: 'slideDownKF 0.5s ease-out'
 		}
 	},
   },
