89a4fdf18917fe6600a10aa8977a639830452848
User Friendly <pivanov@user-friendly.dev>
2024-10-10 16:13:22 -0500

    Promote Inputs and Forms lesson components.

---
+++ src/Components/Form.jsx
@@ -0,0 +1,16 @@
+import _ from 'lodash'
+
+export default (props) => {
+	let newProps = _.assign({}, props)
+	delete newProps.preventDefault
+	newProps.method = _.isString(newProps.method) ? newProps.method : 'POST'
+	newProps.action = _.isString(newProps.action) ? newProps.action : '/'
+	// Default is to prevent default submit behaviour.
+	if (props.preventDefault === undefined || props.preventDefault == true) {
+		newProps.onSubmit = (e) => {
+			e.preventDefault()
+			_.isFunction(props.onSubmit) && props.onSubmit(e)
+		}
+	}
+	return <form {...newProps}>{newProps.children}</form>
+}
