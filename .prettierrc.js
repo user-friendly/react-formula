dd6ec48493dffecbaad4beba2ea51ddb83aff5f4
User Friendly <pivanov@user-friendly.dev>
2024-10-11 14:53:16 -0500

    Add Prettier and set project configs.
    Tabs, yes.

---
+++ .prettierrc.js
@@ -0,0 +1,17 @@
+/**
+ * @see https://prettier.io/docs/en/configuration.html
+ * @type {import("prettier").Config}
+ */
+
+const config = {
+  trailingComma: 'es5',
+  bracketSpacing: false,
+  bracketSameLine: false,
+  arrowParens: 'always',
+  useTabs: true,
+  tabWidth: 2,
+  semi: false,
+  singleQuote: true,
+};
+
+export default config;
