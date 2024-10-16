addfeeb05ad840caaad30bc42e0e04200dc0b18c
User Friendly <pivanov@user-friendly.dev>
2024-10-16 14:52:15 -0500

    Refactor NavBar.

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -6,20 +6,7 @@ const NavBar = ({children}) => {
 	console.log('Render NavBar component.')
 
 	return (
-		<div
-			className="
-		text-lg
-		font-semibold
-		font-mono
-		
-		text-stone-800
-		
-		p-2
-		
-		flex flex-row flex-wrap gap-2
-		justify-center
-  	 "
-		>
+		<div className="text-lg font-semibold font-mono text-stone-800 p-2 flex flex-row flex-wrap gap-2 justify-center">
 			{children}
 		</div>
 	)
