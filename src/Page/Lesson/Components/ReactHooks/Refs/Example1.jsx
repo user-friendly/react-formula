30aacb0dc71f54ec5a07c0b74ecc7981b5f81004
User Friendly <pivanov@user-friendly.dev>
2024-10-24 15:43:33 -0500

    Complete Refs lesson.

--- src/Page/Lesson/Components/ReactHooks/Refs/Example1.jsx
+++ src/Page/Lesson/Components/ReactHooks/Refs/Example1.jsx
@@ -1,14 +1,23 @@
+
+import {useRef} from 'react'
+
 const Example1 = () => {
+	const dogRef = useRef(null)
+	
+	const handleClick = () => {
+		dogRef.current.scrollIntoView({behavior: 'smooth', block: 'center'})
+	}
+	
 	return (
 		<div className="relative h-screen bg-violet-100">
 			<h2 className="p-8 text-3xl font-bold text-violet-700">Example 1</h2>
 			<div className="flex justify-center">
-				<button className="px-8 py-2 mx-8 text-white bg-purple-500 rounded-full">
+				<button className="px-8 py-2 mx-8 text-white bg-purple-500 rounded-full" onClick={handleClick}>
 					scroll to dog
 				</button>
 			</div>
-			<div className="absolute bottom-8 left-8">
-				<i className="text-5xl fa-solid fa-dog"></i>
+			<div ref={dogRef} className="absolute bottom-8 left-8">
+				<span className="text-5xl">🐕</span>
 			</div>
 		</div>
 	)
--- src/Page/Lesson/Components/ReactHooks/Refs/Example2.jsx
+++ src/Page/Lesson/Components/ReactHooks/Refs/Example2.jsx
@@ -1,12 +1,21 @@
+
+import {useRef} from 'react'
+
 const Example2 = () => {
+	const inputRef = useRef(null)
+	
+	const handleClick = () => {
+		inputRef.current.focus()
+	}
+	
 	return (
 		<div className="flex flex-col h-screen bg-yellow-100">
 			<h2 className="p-8 text-3xl font-bold text-amber-700">Example 2</h2>
 			<div className="flex items-center justify-center flex-1">
-				<button className="px-8 py-2 mx-8 text-white rounded-full bg-amber-600">
+				<button onClick={handleClick} className="px-8 py-2 mx-8 text-white rounded-full bg-amber-600">
 					click to focus on input
 				</button>
-				<input
+				<input ref={inputRef}
 					className="px-4 py-2 bg-yellow-200 border-2 border-yellow-400 rounded-md w-80"
 					placeholder="this is an input"
 				/>
