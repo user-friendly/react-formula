44bc10ca35e1cad386ff80689ab7bd7d1120b702
User Friendly <pivanov@user-friendly.dev>
2024-10-26 20:57:29 -0500

    Implement MRD hero section styling.

--- src/Standalone/MobileResponsiveDesign/Hero.jsx
+++ src/Standalone/MobileResponsiveDesign/Hero.jsx
@@ -2,14 +2,18 @@
 import {Link} from 'react-router-dom'
 
 const Hero = () => {
-	return <div className="flex justify-evenly">
-		<div className="max-w-80">
-			Something <span className="text-amber-400">Catchy</span> and <span className="text-red-400">Technological</span>
-			<Link to="#" className="">Learn More</Link>
-		</div>
-		
-		<div className="max-w-80">
-			<img src="https://static-task-assets.react-formula.com/963190.png" title="Hashtag" />
+	return <div className="flex justify-center">
+		<div className="w-full max-w-6xl md:py-32 px-6 flex flex-col-reverse md:flex-row justify-center items-center">
+			<div className="md:max-w-[450px] md:text-5xl text-4xl font-ubuntu font-medium ">
+				Something <span className="text-yellow-500">Catchy</span> and <span className="text-red-600">Technological</span>
+				<br />
+				<Link to="#" className="
+					inline-block my-10 py-3 px-12 border-4 border-violet-800 font-medium text-2xl text-violet-600
+				">Learn More</Link>
+			</div>
+			
+			<img className="mt-28 mb-16 md:w-[420px] md:m-0"
+			 src="https://static-task-assets.react-formula.com/963190.png" title="Hashtag" />
 		</div>
 	</div>
 }
--- src/Standalone/MobileResponsiveDesign/NavBar.jsx
+++ src/Standalone/MobileResponsiveDesign/NavBar.jsx
@@ -5,14 +5,22 @@ const linkStyle = "mx-4 text-xl font-medium hover:underline"
 
 const NavBar = (props) => {
 	return <div className="mb-8 flex justify-center shadow-lg bg-violet-50"> 
-		<div className="w-full max-w-6xl py-4 flex justify-center items-center
+		<div className="w-full max-w-5xl py-4 px-4 flex justify-center items-center
 		">
-			<img className="w-24" src="https://static-task-assets.react-formula.com/899963.png" title="hashtag" />
-			<h1 className="flex-1 ml-4 font-ubuntu text-4xl ">hasher</h1>
-			<Link className={linkStyle} to="/">Home</Link>
-			<Link className={linkStyle} to="/about">About</Link>
-			<Link className={linkStyle} to="/contact">Contact</Link>
-			<Link className={linkStyle} to="https://www.duckduckgo.com/">🔍</Link>
+			<img className="w-20 md:w-24" src="https://static-task-assets.react-formula.com/899963.png" title="hashtag" />
+			<h1 className="ml-4 font-ubuntu text-4xl">hasher</h1>
+			
+			<div className="flex-1 flex justify-end ">
+				<div className="md:hidden text-4xl">
+					🍔
+				</div>
+				<div className="hidden md:block">
+					<Link className={linkStyle} to="/">Home</Link>
+					<Link className={linkStyle} to="/about">About</Link>
+					<Link className={linkStyle} to="/contact">Contact</Link>
+					<Link className={linkStyle} to="https://www.duckduckgo.com/">🔍</Link>
+				</div>
+			</div>
 		</div>
 	</div>
 }
