5b8767e961e6c0360efdada1a6a69cbafe5d9aaa
User Friendly <pivanov@user-friendly.dev>
2024-11-04 19:24:29 -0500

    Finish up Capstone auth form pages.

--- src/Standalone/Capstone/Pages/Auth/FormContainer.jsx
+++ src/Standalone/Capstone/Pages/Auth/FormContainer.jsx
@@ -2,15 +2,24 @@
 const FormContainer = (props) => {
 	const children = props.children
 	
-	return <div className="h-screen bg-green-50 flex flex-col items-center justify-center">
-		<div className="flex flex-col items-center mx-2 my-8">
-			<img className="w-16 mb-2 " title="brand logo" src="https://static-task-assets.react-formula.com/capstone_logo_dark.png" />
-			<div className="font-playfair text-emerald-700 text-2xl">
-				Rica's Plants
-			</div>
+	return<div className="flex">
+		<div className="relative hidden md:block">
+			<img className="h-screen object-cover"
+				title="plants on a rack" src="https://static-task-assets.react-formula.com/capstone_sign_in_scene.png" />
+			<div className="absolute inset-0 w-full h-full bg-black/10"></div>
+			<div className="absolute inset-0 w-full h-full bg-green-800/40"></div>
 		</div>
 		
-		{children}
+		<div className="h-screen flex-1 bg-green-50 flex flex-col items-center justify-center">
+			<div className="flex flex-col items-center mx-2 my-8">
+				<img className="w-16 mb-2 " title="brand logo" src="https://static-task-assets.react-formula.com/capstone_logo_dark.png" />
+				<div className="font-playfair text-emerald-700 text-2xl">
+					Rica's Plants
+				</div>
+			</div>
+			
+			{children}
+		</div>
 	</div>
 }
 
