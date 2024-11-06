64539ec03a74d17ff8a0a88a56159a702729bc2e
User Friendly <pivanov@user-friendly.dev>
2024-11-05 19:13:37 -0500

    Temporarily enable old Capstone NavBar.

--- src/Standalone/Capstone/App.jsx
+++ src/Standalone/Capstone/App.jsx
@@ -2,11 +2,13 @@
 import {BrowserRouter, Link, Routes, Route} from 'react-router-dom'
 
 import RoutesMap from '#cap/RoutesMap'
+import NavBar from '#cap/NavBar'
 
 const globalFontStyle = "text-green-600"
 
 const App = () => {
 	return <BrowserRouter>
+		<NavBar />
 		<div className={`${globalFontStyle}`}>
 			<RoutesMap />
 		</div>
