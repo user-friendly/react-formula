e90448b0b08769a338d6528f8dd0aa60a12e545d
User Friendly <pivanov@user-friendly.dev>
2024-11-09 17:37:54 -0500

    Capstone: Create & add session context to App.

--- src/Standalone/Capstone/App.jsx
+++ src/Standalone/Capstone/App.jsx
@@ -1,5 +1,9 @@
 
+import {useState} from 'react'
 import {BrowserRouter, Link, Routes, Route} from 'react-router-dom'
+
+import {getSessionStorage} from '#cap/Services'
+import SessionContext from '#cap/Context/Session'
 
 import RoutesMap from '#cap/RoutesMap'
 import NavBar from '#cap/NavBar'
@@ -7,12 +11,16 @@ import NavBar from '#cap/NavBar'
 const globalFontStyle = "text-green-600"
 
 const App = () => {
-	return <BrowserRouter>
-		<div className={`${globalFontStyle}`}>
-			<RoutesMap />
-		</div>
-		<NavBar />
-	</BrowserRouter>
+	const [session, setSession] = useState(() => getSessionStorage())
+	
+	return <SessionContext.Provider value={session}>
+		<BrowserRouter>
+			<div className={`${globalFontStyle}`}>
+				<RoutesMap />
+			</div>
+			<NavBar />
+		</BrowserRouter>
+	</SessionContext.Provider>
 }
 
 export default App
---
+++ src/Standalone/Capstone/Context/Session.js
@@ -0,0 +1,6 @@
+
+import {createContext} from 'react'
+
+const SessionContext = createContext(false)
+
+export default SessionContext
