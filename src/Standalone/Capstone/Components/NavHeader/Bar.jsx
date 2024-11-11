3bff2dd44ea97fb1659457189e347cb1221f706a
User Friendly <pivanov@user-friendly.dev>
2024-11-11 18:54:00 -0500

    Capstone: Refactor nav header bar to use icons.

--- src/Standalone/Capstone/Components/NavHeader/Bar.jsx
+++ src/Standalone/Capstone/Components/NavHeader/Bar.jsx
@@ -5,14 +5,18 @@ import clsx from 'clsx'
 
 import SessionContext from '#cap/Context/Session'
 
-import Spinner from '#cap/Components/Spinner'
 import Icon from '#cap/Components/Icon'
 
 import Links from './Links'
 
 const navBarStyle = `fixed top-2 right-2 w-40 p-2 flex flex-col justify-center`
 
-const linkStyle = `p-2 font-lato font-medium hover:text-emerald-200 hover:underline`
+const linkStyle = `p-2 font-lato font-medium hover:text-emerald-200 hover:underline active:translate-y-0.5`
+const linkIconStyle = `mx-1`
+
+// TODO Might want to change the default icon style to match this one.
+const iconStyle = `bg-transparent text-white hover:bg-emerald-700 active:bg-emerald-600`
+const iconProgressStyle = `${iconStyle} rounded-full animate-spin`
 
 const spinnerStyle = `ml-1 w-4 h-4 inline-block
 	border-transparent border-t-white group-hover:border-t-emerald-200
@@ -33,12 +37,12 @@ const Bar = () => {
 			navigate('/')
 		}
 		// 2 sec delay
-		//await (new Promise((r) => setTimeout(() => r(), 1000)).then((r) => r))
+		//await (new Promise((r) => setTimeout(() => r(), 2500)).then((r) => r))
 		setSigningOut(false)
 	}
 	
-	const signOutButton = <Link onClick={handleSignOut} className={linkStyle + ' relative group'}>
-		Sign Out <Spinner className={spinnerStyle + (!signingOut && ' invisible')} />
+	const signOutButton = <Link onClick={handleSignOut} className={linkIconStyle + ' relative group'}>
+		{signingOut && <Icon name="progress_activity" className={iconProgressStyle} /> || <Icon name="logout" className={iconStyle} />}
 	</Link>
 	
 	return <div className="hidden sm:flex justify-center bg-emerald-800">
@@ -50,9 +54,9 @@ const Bar = () => {
 			</div>	
 			
 			<Links linkstyle={linkStyle} />
-					
+			
 			{session.isActive() && signOutButton}
-			{!session.isActive() && <Link className={linkStyle} to="/sign-in">Sign In</Link>}
+			{!session.isActive() && <Link className={linkIconStyle} to="/sign-in"><Icon name="login" className={iconStyle}/></Link>}
 		</div>
 	</div>
 }
