66c5ee84e894e5ce991a23fd71d901535c616280
User Friendly <pivanov@user-friendly.dev>
2024-11-12 17:15:26 -0500

    Capstone: Initial Plants List page API integration

--- src/Standalone/Capstone/Pages/PlantList/Page.jsx
+++ src/Standalone/Capstone/Pages/PlantList/Page.jsx
@@ -1,9 +1,27 @@
 
-import RedirectAuthenticated from '#cap/Components/RedirectAuthenticated'
+import {useContext, useEffect, useState} from 'react'
 
+import {ApiGetPlants} from  '#cap/Services'
+import RedirectAuthenticated from '#cap/Components/RedirectAuthenticated'
 import NavHeader from '#cap/Components/NavHeader'
 
+import SessionContext from '#cap/Context/Session'
+
 const Page = () => {
+	const session = useContext(SessionContext)
+	const [list, setList] = useState([])
+	
+	useEffect(() => {
+		(async () => {
+			const result = await ApiGetPlants(session?.data)
+			if (result.error === false) {
+				console.log(result)
+			} else {
+				console.error(`Failed to get plants list. Response:`, result)
+			}
+		})()
+	}, [session.data])
+	
 	return <RedirectAuthenticated not path="/sign-in">
 		<NavHeader />
 		<div className="h-screen flex flex-col justify-center items-center">
