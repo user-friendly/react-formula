c36a9f9947f696e782c379afe42349e178e1a8f0
User Friendly <pivanov@user-friendly.dev>
2024-11-12 17:14:57 -0500

    Capstone: Add fetch Plants List service.

---
+++ src/Standalone/Capstone/Services/Plants.js
@@ -0,0 +1,15 @@
+
+import {default as ApiFetch, processStatus, SESSION_TOKEN_HEADER} from './ApiFetch'
+
+export const ApiGetPlants = async (sessionData) => {
+	try {
+		const sessionToken = JSON.stringify(sessionData)
+		const r = await ApiFetch('GET', 'plants', {
+			SESSION_TOKEN_HEADER: sessionToken,
+		})
+		return processStatus(r)
+	} catch (e) {
+		console.error('Failed to get plants list.')
+	}
+	return false
+}
