35565c58256a8ae6a11fe26307acc5bdfffab111
User Friendly <pivanov@user-friendly.dev>
2024-11-12 20:42:08 -0500

    Capstone: Plants List API integration.

--- src/Standalone/Capstone/Pages/PlantList/Page.jsx
+++ src/Standalone/Capstone/Pages/PlantList/Page.jsx
@@ -4,28 +4,58 @@ import {useContext, useEffect, useState} from 'react'
 import {ApiGetPlants} from  '#cap/Services'
 import RedirectAuthenticated from '#cap/Components/RedirectAuthenticated'
 import NavHeader from '#cap/Components/NavHeader'
+import Spinner from '#cap/Components/Spinner'
 
 import SessionContext from '#cap/Context/Session'
 
 const Page = () => {
 	const session = useContext(SessionContext)
-	const [list, setList] = useState([])
+	const [list, setList] = useState(null)
+	const [status, setStatus] = useState({error: false})
+	
+	const refreshList = async () => {
+		setList(null)
+		const result = await ApiGetPlants(session?.data)
+		if (result.error === false) {
+			setList(result.data)
+		} else {
+			console.error(`Failed to get plants list. Response:`, result)
+		}
+		setStatus(result)
+	}
 	
 	useEffect(() => {
-		(async () => {
-			const result = await ApiGetPlants(session?.data)
-			if (result.error === false) {
-				console.log(result)
-			} else {
-				console.error(`Failed to get plants list. Response:`, result)
-			}
-		})()
+		refreshList()
 	}, [session.data])
+	
+	const PlantItem = (props) => {
+		const {data} = props
+		
+		return <div className="my-4 p-3 border rounded-lg">
+			<div>{data.name}</div>
+			
+			<img className="my-4 max-w-48 object-cover rounded-md" alt={data.description} src={data.image} />
+			
+			<div>
+				{data.description}
+			</div>	
+		</div>
+	}
 	
 	return <RedirectAuthenticated not path="/sign-in">
 		<NavHeader />
-		<div className="h-screen flex flex-col justify-center items-center">
-			Plant List
+		<div className="py-12 flex flex-col justify-center items-center">
+			{status?.error !== false && (
+				<div className="mb-8 px-2 py-1 bg-rose-100 border border-rose-300 rounded-lg text-red-600 font-medium">
+					{status.message}
+				</div>
+			)}
+			<div>
+				{(list && list.map((plant, key) => <PlantItem key={key} data={plant} />))
+					|| (status?.error === false && <Spinner />)}
+				<br />
+				<button onClick={refreshList} className="px-2 py-1 border rounded-md bg-emerald-50">Refresh</button>
+			</div>
 		</div>
 	</RedirectAuthenticated>
 }
