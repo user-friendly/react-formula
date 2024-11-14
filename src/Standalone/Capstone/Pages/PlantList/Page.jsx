dc96225a279052f381edcf255b08cec0b450e9e8
User Friendly <pivanov@user-friendly.dev>
2024-11-14 12:28:32 -0500

    Capstone: Style Plant List page.

--- src/Standalone/Capstone/Pages/PlantList/Page.jsx
+++ src/Standalone/Capstone/Pages/PlantList/Page.jsx
@@ -1,12 +1,16 @@
 
 import {useContext, useEffect, useState} from 'react'
 
+import SessionContext from '#cap/Context/Session'
 import {ApiGetPlants} from  '#cap/Services'
+
 import RedirectAuthenticated from '#cap/Components/RedirectAuthenticated'
+
 import NavHeader from '#cap/Components/NavHeader'
 import Spinner from '#cap/Components/Spinner'
+import {Section, Heading} from '#cap/Components/Text'
 
-import SessionContext from '#cap/Context/Session'
+import PlantItem from './PlantItem'
 
 const Page = () => {
 	const session = useContext(SessionContext)
@@ -28,35 +32,27 @@ const Page = () => {
 		refreshList()
 	}, [session.data])
 	
-	const PlantItem = (props) => {
-		const {data} = props
-		
-		return <div className="my-4 p-3 border rounded-lg">
-			<div>{data.name}</div>
-			
-			<img className="my-4 max-w-48 object-cover rounded-md" alt={data.description} src={data.image} />
-			
-			<div>
-				{data.description}
-			</div>	
-		</div>
-	}
+	const spinner = <Spinner />
+	
+	
 	
 	return <RedirectAuthenticated not path="/sign-in">
 		<NavHeader />
-		<div className="py-12 flex flex-col justify-center items-center">
+		<Section className="py-24 flex justify-center">
 			{status?.error !== false && (
 				<div className="mb-8 px-2 py-1 bg-rose-100 border border-rose-300 rounded-lg text-red-600 font-medium">
 					{status.message}
 				</div>
 			)}
-			<div>
-				{(list && list.map((plant, key) => <PlantItem key={key} data={plant} />))
-					|| (status?.error === false && <Spinner />)}
-				<br />
-				<button onClick={refreshList} className="px-2 py-1 border rounded-md bg-emerald-50">Refresh</button>
+			
+			<div className="w-full max-w-5xl">
+				<Heading className="text-4xl">Plants In Stock</Heading>
+				<div className="flex flex-wrap justify-center">
+					{(list && list.map((plant, i) => <PlantItem key={plant.id} data={plant} />))
+						|| (status?.error === false && spinner)}
+				</div>
 			</div>
-		</div>
+		</Section>
 	</RedirectAuthenticated>
 }
 
---
+++ src/Standalone/Capstone/Pages/PlantList/PlantItem.jsx
@@ -0,0 +1,64 @@
+
+import _ from 'lodash'
+
+import {useState} from 'react'
+
+import clsx from 'clsx'
+
+const POT_COLORS = {
+  stone: 'bg-stone-200', // #e7e5e4
+  slate: 'bg-slate-300', // #cbd5e1
+  sky: 	 'bg-sky-700',	 // #0369a1
+  black: 'bg-gray-600',	 // #4b5563
+  white: 'bg-gray-50',	 // #f9fafb
+  amber: 'bg-amber-600', // #d97706
+}
+
+const TEXT_COLORS = {
+  stone: 'text-stone-400', // #e7e5e4
+  slate: 'text-slate-500', // #cbd5e1
+  sky: 	 'text-sky-700',   // #0369a1
+  black: 'text-gray-600',  // #4b5563
+  white: 'text-gray-400',  // #f9fafb
+  amber: 'text-amber-600', // #d97706
+}
+
+const PlantItem = (props) => {
+	const [selected, setSelected] = useState(_.random(0, props.data.images.length-1))
+	const [imageIdx, setImageIdx] = useState(selected)
+	
+	const {data} = props
+	const price = Number(data.price).toFixed(2)
+	const color = data.images[imageIdx].pot_color
+	
+	return <div className="mx-5 my-8">
+		<img className="w-[280px] h-[320px] object-cover rounded-md"
+			alt={data.description} src={data.images[imageIdx].src} />
+
+		<div className="my-3 max-w-[280px]">
+			{data.description}
+		</div>
+			
+		<div className="flex justify-between">
+			<div className="text-xl text-emerald-700">{data.name}</div>
+			<div className="text-lg text-emerald-600">${price}</div>
+		</div>
+		
+		<div className="flex justify-between my-3">
+			<div className={TEXT_COLORS[color] + ' font-medium'}>{color}</div>
+			<div className="flex justify-end">
+				{data.images.map((image, i) => <button key={i} className={clsx(
+					'flex justify-center items-center ml-2 w-5 h-5 rounded-full block ',
+					selected === i && 'outline outline-1 outline-offset-2 outline-slate-400',
+					POT_COLORS[data.images[i].pot_color]
+				)}
+					onClick={() => {setSelected(i); setImageIdx(i)}}
+					onMouseEnter={() => setImageIdx(i)}
+					onMouseLeave={() => setImageIdx(selected)}
+				/>)}
+			</div>
+		</div>
+	</div>
+}
+
+export default PlantItem
