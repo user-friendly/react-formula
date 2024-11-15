eaa86f33b12c47908e9186108ad0708e6e61e1e7
User Friendly <pivanov@user-friendly.dev>
2024-11-15 14:08:12 -0500

    Capstone: Add new icons, add decorative Icon.

--- src/Standalone/Capstone/Components/Icon.jsx
+++ src/Standalone/Capstone/Components/Icon.jsx
@@ -8,6 +8,8 @@ const iconStyle = `
 	hover:bg-emerald-200 active:bg-emerald-300 text-emerald-700
 `
 
+const iconDecorativeStyle = `text-[2rem] p-2 select-none text-emerald-700`
+
 // TODO Is this an Icon or is it a button?
 
 const Icon = (props) => {
@@ -21,4 +23,16 @@ const Icon = (props) => {
 	return <span className={'material-symbols-outlined ' + twMerge(iconStyle, className)} {...newProps}>{name}</span>
 }
 
+const IconDecorative = (props) => {
+	if (props.name === undefined) {
+		return <></>
+	}
+	const name = props.name
+	const className = props.className
+	const newProps = _.omit(_.clone(props), ['name', 'className'])
+	// material-symbols-outlined class is always on.
+	return <span className={'material-symbols-outlined ' + twMerge(iconDecorativeStyle, className)} {...newProps}>{name}</span>
+}
+
+export {IconDecorative}
 export default Icon
