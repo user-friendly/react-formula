3ca4a065e970441aedd12858e5aac86962e2d422
User Friendly <pivanov@user-friendly.dev>
2024-11-17 14:45:27 -0500

    Capstone: Add cart page link to generic nav links.

--- src/Standalone/Capstone/Components/NavHeader/Links.jsx
+++ src/Standalone/Capstone/Components/NavHeader/Links.jsx
@@ -1,10 +1,27 @@
 
 import {Link, useNavigate} from 'react-router-dom'
+
 import {RequireSession} from '#cap/Components/AccessControl'
 
+import Icon from '#cap/Components/Icon'
+
+const iconStyle = `
+	bg-transparent text-white
+	hover:bg-emerald-700 group-hover:bg-emerald-700 active:bg-emerald-600 group-active:bg-emerald-600
+`
+
+const linkHasIconStyle = `group hover:no-underline flex items-center`
+const linkHasIconTextStyle = `mr-1 group-hover:underline`
+
 const Links = ({linkstyle}) => {
+	// Horrible naming.
+	const linkStyle = linkstyle + ' ' + linkHasIconStyle
+	
 	return <RequireSession>
 		<Link className={linkstyle} to="/plants">Plant List</Link>
+		<Link className={linkStyle} to="/cart">
+			<span className={linkHasIconTextStyle}>Cart</span><Icon className={iconStyle} name="shopping_cart" />
+		</Link>
 	</RequireSession>
 }
 
