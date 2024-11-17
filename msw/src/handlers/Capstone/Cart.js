1f6661719dc58c9c0125cb781bc2a3cd65cbbebb
User Friendly <pivanov@user-friendly.dev>
2024-11-17 13:52:59 -0500

    Capstone: MSW, add Cart service.

---
+++ msw/src/handlers/Capstone/Cart.js
@@ -0,0 +1,182 @@
+
+import _ from 'lodash'
+
+import {http, delay, HttpResponse} from 'msw'
+import {v4 as uuidv4} from 'uuid'
+
+import {hasAccessTo} from './Users'
+
+const randomDelay = async (min = 0) => await delay(min + _.random(250, 1000))
+
+// Plants table.
+const store = new Map()
+
+// Add some dummy cart items.
+store.set('johndoe123', [
+	{plant: {
+		quantity: 2,
+		uuid: '1a1d6735-be08-4d34-9736-2e03da2c9d98',
+		pot_color: 'black',
+	}},
+	{plant: {
+		quantity: 1,
+		uuid: '4e9482ac-c661-4c77-8755-be50376b12ef',
+		pot_color: 'sky',
+	}},
+	{plant: {
+		quantity: 1,
+		uuid: '1a1d6735-be08-4d34-9736-2e03da2c9d98',
+		pot_color: 'sky',
+	}},
+])
+
+const Plants = (baseUrl) => {
+	return [
+		http.get(`${baseUrl}/cart`, async ({cookies, request}) => {
+			
+			await randomDelay()
+			
+			if (!hasAccessTo(request, 'get cart items')) {
+				return HttpResponse.json({
+						error: 'Access forbidden.',
+						code: 1,
+					},
+					{status: 401}
+				)
+			}
+			
+			const session = getSessionFromRequest(request)
+			
+			let cart = []
+			if (store.has(session.username)) {
+				cart = Array.from(store.get(session.username))
+			}
+			
+			return HttpResponse.json(
+				{
+					data: cart,
+					code: 0,
+				},
+				{status: 200}
+			)
+		}),
+		http.get(`${baseUrl}/cart/:uuid`, async ({cookies, request, params}) => {
+			const {uuid} = params
+			// An array of item(s) to add to cart.
+			// Each object is keyed by item type.
+			// Example of a plant item:
+			// 
+			// [{
+			// 		plant: {
+			// 			uuid: '550e8400-e29b-41d4-a716-446655440000',
+			// 			quantity: 1,
+			// 			pot_color: 'amber',
+			// 		}
+			// }]
+			//
+			const item = await request.json()
+			
+			if (!hasAccessTo(request, 'add cart items')) {
+				return HttpResponse.json({
+						error: 'Access forbidden.',
+						code: 2,
+					},
+					{status: 401}
+				)
+			}
+			const session = getSessionFromRequest(request)
+			
+			if (!_isArray(items) || .isEmpty(items)) {
+				return HttpResponse.json({
+						error: 'Invalid request',
+						code: 3,
+					},
+					{status: 200/*401*/}
+				)
+			}
+			
+			const cartList = store.has(session.username) ? store.get(session.username) : []
+			for (const item of items) {
+				cartList.push(item)
+			}
+			store.set(session.username, cartList)
+			
+			return HttpResponse.json(
+				{
+					message: 'Item added to cart successfully.',
+					code: 0,
+				},
+				{status: 200}
+			)
+		}),
+		http.delete(`${baseUrl}/cart`, async ({cookies, request}) => {
+			if (!hasAccessTo(request, 'add cart items')) {
+				return HttpResponse.json({
+						error: 'Access forbidden.',
+						code: 2,
+					},
+					{status: 401}
+				)
+			}
+			const session = getSessionFromRequest(request)
+			
+			// Clears cart.
+			store.set(session.username, [])
+			
+			return HttpResponse.json(
+				{
+					message: 'Cart cleared successfully',
+					code: 0,
+				},
+				{status: 200}
+			)
+		}),
+		http.delete(`${baseUrl}/cart/:offset`, async ({cookies, request, params}) => {
+			const {offset} = params
+			
+			if (!hasAccessTo(request, 'add cart items')) {
+				return HttpResponse.json({
+						error: 'Access forbidden.',
+						code: 2,
+					},
+					{status: 401}
+				)
+			}
+			
+			const session = getSessionFromRequest(request)
+			const cartList = store.has(session.username) ? store.get(session.username) : []
+			if (cartList[offset] !== undefined) {
+				cartList.splice(offset, 1)
+				store.set(session.username, cartList)
+			} else {
+				// NOTE Notify user?
+			}
+			
+			return HttpResponse.json(
+				{
+					message: 'Item removed from cart successfully',
+					code: 0,
+				},
+				{status: 200}
+			)
+		}),
+	]
+}
+
+export default Plants
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- msw/src/handlers/Capstone/index.js
+++ msw/src/handlers/Capstone/index.js
@@ -1,10 +1,12 @@
 
 import UsersHandler from './Users'
 import PlantsHandler from './Plants'
+import CartHandler from './Cart'
 
 const ENDPOINT_BASE_URL = `${import.meta.env.VITE_API_BASE_URL}/learning-api/demos/capstone`
 
 export default [
 	...UsersHandler(ENDPOINT_BASE_URL),
 	...PlantsHandler(ENDPOINT_BASE_URL),
+	...CartHandler(ENDPOINT_BASE_URL),
 ]
