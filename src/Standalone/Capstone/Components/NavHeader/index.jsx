696abf2835e3e54703b0ddb6fcfc09c6456c9059
User Friendly <pivanov@user-friendly.dev>
2024-11-22 12:35:56 -0500

    Capstone: Fix width of header logo.

--- src/Standalone/Capstone/Components/NavHeader/index.jsx
+++ src/Standalone/Capstone/Components/NavHeader/index.jsx
@@ -15,7 +15,7 @@ export default () => {
 		<nav className="navbar flex justify-center bg-emerald-800">
 			<div className="w-full max-w-5xl flex p-8 justify-end items-center text-white">
 				
-				<div className="flex-1 flex">
+				<div className="min-w-64 flex-1 flex">
 					<Link to="/" className="p-2 flex justify-start items-center">
 						<img className="w-10" title="brand logo"
 							src="https://static-task-assets.react-formula.com/capstone_logo_light.png" />
