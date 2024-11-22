531f9fe66a8901fd753669a433d4b22fb29bbcd5
User Friendly <pivanov@user-friendly.dev>
2024-11-22 12:25:55 -0500

    Capstone: Move bar component into nav index.

--- src/Standalone/Capstone/Components/NavHeader/index.jsx
+++ src/Standalone/Capstone/Components/NavHeader/index.jsx
@@ -1,13 +1,36 @@
 
-import Bar from './Bar'
+import {Link} from 'react-router-dom'
+
+import Links from './Links'
 import Ham from './Ham'
 import {Modal as CartModal} from '../Cart'
 import NavBarUserFriendly from '../NavBarUserFriendly'
 
+const Bar = () => {
+	return 
+}
+
 export default () => {
 	return <>
-		<Ham />
-		<Bar />
+		<nav className="navbar flex justify-center bg-emerald-800">
+			<div className="w-full max-w-5xl flex p-8 justify-end items-center text-white">
+				
+				<div className="flex-1 flex">
+					<Link to="/" className="p-2 flex justify-start items-center">
+						<img className="w-10" title="brand logo"
+							src="https://static-task-assets.react-formula.com/capstone_logo_light.png" />
+						<span className="ml-4 font-playfair text-2xl">Rica's Plants</span>
+					</Link>
+				</div>
+				
+				<div className="hidden sm:flex flex-wrap justify-end">
+					<Links />
+				</div>
+				
+				<Ham />
+			</div>
+		</nav>
+		
 		<CartModal />
 		<NavBarUserFriendly />
 	</>
