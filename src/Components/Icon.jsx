0577e2affd902897ca580a580a65b377622057ed
User Friendly <pivanov@user-friendly.dev>
2024-11-29 17:12:15 -0500

    Yank Capstone's icon component to main app.

---
+++ src/Components/Icon.jsx
@@ -0,0 +1,38 @@
+
+import _ from 'lodash'
+
+import {twMerge} from 'tailwind-merge'
+
+const iconStyle = `
+	p-2 rounded-lg bg-gray-100 select-none cursor-pointer
+	hover:bg-gray-200 active:bg-gray-300 text-gray-700
+`
+
+const iconDecorativeStyle = `text-[2rem] p-2 select-none text-gray-700`
+
+// TODO Is this an Icon or is it a button?
+
+const Icon = (props) => {
+	if (props.name === undefined) {
+		return <></>
+	}
+	const name = props.name
+	const className = props.className
+	const newProps = _.omit(_.clone(props), ['name', 'className'])
+	// material-symbols-outlined class is always on.
+	return <span className={'material-symbols-outlined ' + twMerge(iconStyle, className)} {...newProps}>{name}</span>
+}
+
+const IconDecorative = (props) => {
+	if (props.name === undefined) {
+		return <></>
+	}
+	const name = props.name
+	const className = props.className
+	const newProps = _.omit(_.clone(props), ['name', 'className'])
+	// material-symbols-outlined class is always on.
+	return <span className={'material-symbols-outlined ' + twMerge(iconDecorativeStyle, className)} {...newProps}>{name}</span>
+}
+
+export {IconDecorative}
+export default Icon
