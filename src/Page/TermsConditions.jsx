e32f15f524c2250bd7d744cf577b03293faedfc6
User Friendly <pivanov@user-friendly.dev>
2024-12-02 15:16:29 -0500

    Add Terms & Conditions skeli page.

---
+++ src/Page/TermsConditions.jsx
@@ -0,0 +1,22 @@
+/**
+ * Terms & Conditions view.
+ * 
+ * %title = Terms & Conditions
+ * %route = /terms
+ */
+
+import {Section, Heading, Paragraph} from '#Components/Text'
+
+const TermsConditions = () => {
+	return <Section>
+		<Heading className="">Terms & Conditions</Heading>
+		
+		<Paragraph>[filler]</Paragraph>
+		
+		<Paragraph>[filler]</Paragraph>
+
+		<Paragraph>[filler]</Paragraph>
+	</Section>
+}
+
+export default TermsConditions
