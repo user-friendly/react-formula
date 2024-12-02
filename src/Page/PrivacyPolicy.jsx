a6a3ee3197c53d4920d3238c2e6966d74e79485d
User Friendly <pivanov@user-friendly.dev>
2024-12-02 15:15:33 -0500

    Add Privacy Policy skeli page.

---
+++ src/Page/PrivacyPolicy.jsx
@@ -0,0 +1,22 @@
+/**
+ * Privacy Policy view.
+ * 
+ * %title = Privacy Policy
+ * %route = /privacy
+ */
+
+import {Section, Heading, Paragraph} from '#Components/Text'
+
+const PrivacyPolicy = () => {
+	return <Section>
+		<Heading className="">Privacy Policy</Heading>
+		
+		<Paragraph>[filler]</Paragraph>
+		
+		<Paragraph>[filler]</Paragraph>
+
+		<Paragraph>[filler]</Paragraph>
+	</Section>
+}
+
+export default PrivacyPolicy
