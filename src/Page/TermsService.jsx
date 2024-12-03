be65460d467445622755de141962ad396287cfd6
User Friendly <pivanov@user-friendly.dev>
2024-12-02 19:51:14 -0500

    Implement ToS page.

---
+++ src/Page/TermsService.jsx
@@ -0,0 +1,16 @@
+/**
+ * Terms of Service view.
+ * 
+ * %title = Terms of Service
+ * %route = /terms
+ */
+
+import LegaleseMarkdown from '#Components/LegaleseMarkdown'
+
+import MD_TOS from '#Data/EN-Terms-of-Service.md?raw'
+
+const TermsService = () => {	
+	return <LegaleseMarkdown className="m-auto p-4 max-w-5xl">{MD_TOS}</LegaleseMarkdown>
+}
+
+export default TermsService
