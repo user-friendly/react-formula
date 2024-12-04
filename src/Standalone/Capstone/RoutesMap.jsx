a5634edc563c0a8e7d3aa621b5dda6d22128c626
User Friendly <pivanov@user-friendly.dev>
2024-12-04 10:14:48 -0500

    Set temp document title for Rica's Plants app.

--- src/Standalone/Capstone/RoutesMap.jsx
+++ src/Standalone/Capstone/RoutesMap.jsx
@@ -1,4 +1,5 @@
 
+import {useEffect} from 'react'
 import {Routes, Route, Navigate, Outlet, useLocation} from 'react-router'
 
 import NotFound from '#cap/Pages/NotFound'
@@ -24,6 +25,13 @@ const ExampleLayout = () => {
 const RoutesMap = () => {
 	const loc = useLocation()
 	const session = useContext(SessionContext)
+	
+	useEffect(() => {
+		const title = "Rica's Plants"
+		if (title) {
+			document.title = title
+		} // TODO Report to GA?
+	}, [loc])
 	
 	return <Routes>
 		<Route element={<ExampleLayout />}>
